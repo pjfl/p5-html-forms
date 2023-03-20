64b17be4e47a82a1f8a7ed43e97b6c5d9ef9c603
Lazarus Long <github@roxsoft.co.uk>
2023-03-20 09:25:58 +0000

    Switched to using less

--- lib/HTML/Forms.pm
+++ lib/HTML/Forms.pm
@@ -1,7 +1,7 @@
 package HTML::Forms;
 
 use 5.010001;
-use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 27 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 28 $ =~ /\d+/gmx );
 
 use Data::Clone            qw( clone );
 use HTML::Forms::Constants qw( EXCEPTION_CLASS FALSE TRUE NUL );
