68522d528b848d8c45d8cf424e16d89a65a71a08
Lazarus Long <github@roxsoft.co.uk>
2023-04-02 15:08:12 +0100

    Adds timezone field

---
+++ lib/HTML/Forms/Field/Timezone.pm
@@ -0,0 +1,91 @@
+package HTML::Forms::Field::Timezone;
+
+use DateTime::TimeZone;
+use HTML::Forms::Constants qw( META );
+use Moo;
+use HTML::Forms::Moo;
+
+extends 'HTML::Forms::Field::Select';
+
+has '+wrapper_class' => default => 'input-select-timezone';
+
+sub build_options {
+   return [
+      map { +{ label => $_, value => $_ } } DateTime::TimeZone->all_names
+   ];
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 Name
+
+HTML::Forms::Field::Timezone - Generates markup for and processes input from HTML forms
+
+=head1 Synopsis
+
+   use HTML::Forms::Field::Timezone;
+
+=head1 Description
+
+=head1 Configuration and Environment
+
+Defines the following attributes;
+
+=over 3
+
+=back
+
+=head1 Subroutines/Methods
+
+=head1 Diagnostics
+
+=head1 Dependencies
+
+=over 3
+
+=item L<DateTime>
+
+=back
+
+=head1 Incompatibilities
+
+There are no known incompatibilities in this module
+
+=head1 Bugs and Limitations
+
+There are no known bugs in this module. Please report problems to
+http://rt.cpan.org/NoAuth/Bugs.html?Dist=HTML-Forms.
+Patches are welcome
+
+=head1 Acknowledgements
+
+Larry Wall - For the Perl programming language
+
+=head1 Author
+
+Peter Flanigan, C<< <lazarus@roxsoft.co.uk> >>
+
+=head1 License and Copyright
+
+Copyright (c) 2023 Peter Flanigan. All rights reserved
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. See L<perlartistic>
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+=cut
+
+# Local Variables:
+# mode: perl
+# tab-width: 3
+# End:
+# vim: expandtab shiftwidth=3:
