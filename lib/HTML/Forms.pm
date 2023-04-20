6bbc0fae590ad84be070790b821b96369406fa4c
Lazarus Long <github@roxsoft.co.uk>
2023-04-20 01:06:51 +0100

    CSS fonts

--- lib/HTML/Forms.pm
+++ lib/HTML/Forms.pm
@@ -1,7 +1,7 @@
 package HTML::Forms;
 
 use 5.010001;
-use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 38 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 39 $ =~ /\d+/gmx );
 
 use Data::Clone            qw( clone );
 use HTML::Forms::Constants qw( EXCEPTION_CLASS FALSE TRUE NUL );
